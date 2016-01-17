3cda44a69ce313afc1e84f2c0809162a1cda8d15
Robert Kern <rkern@enthought.com>
2016-01-17 22:00:18 +0000

    MAINT: style, setup.cfg

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 import os
+
 from setuptools import setup
 
 kwds = {}
 
 # Read the long description from the README.txt
 thisdir = os.path.abspath(os.path.dirname(__file__))
-f = open(os.path.join(thisdir, 'README.txt'))
-kwds['long_description'] = f.read()
-f.close()
+with open(os.path.join(thisdir, 'README.txt')) as f:
+    kwds['long_description'] = f.read()
 
 
 setup(
-    name = 'grin',
-    version = '1.2.1',
-    author = 'Robert Kern',
-    author_email = 'robert.kern@enthought.com',
-    description = "A grep program configured the way I like it.",
-    license = "BSD",
-    classifiers = [
+    name='grin',
+    version='1.2.1',
+    author='Robert Kern',
+    author_email='robert.kern@enthought.com',
+    description="A grep program configured the way I like it.",
+    license="BSD",
+    url='https://github.com/rkern/grin',
+    classifiers=[
         "License :: OSI Approved :: BSD License",
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
@@ -26,17 +27,16 @@ setup(
         "Programming Language :: Python",
         "Topic :: Utilities",
     ],
-
-    py_modules = ["grin"],
-    entry_points = dict(
-        console_scripts = [
+    py_modules=["grin"],
+    entry_points=dict(
+        console_scripts=[
             "grin = grin:grin_main",
             "grind = grin:grind_main",
         ],
     ),
-    tests_require = [
+    tests_require=[
         'nose >= 0.10',
     ],
-    test_suite = 'nose.collector',
+    test_suite='nose.collector',
     **kwds
 )
